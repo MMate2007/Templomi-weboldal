b5a596b34a222a2bb02b7b4a28c39f16776f2ddf
Menyhárt Máté <72655257+MMate2007@users.noreply.github.com>
2021-09-05 19:42:23 +0200

    Update navbar.php

--- forraskod/navbar.php
+++ forraskod/navbar.php
@@ -1,22 +1,22 @@
 <nav id="desktop">
-<a id="elso" href="http://www.fabianistva.nhely.hu/index.php">Főoldal</a>
-<a href="http://www.fabianistva.nhely.hu/miserend.php">Liturgiák rendje</a>
-<a href="http://www.fabianistva.nhely.hu/history.php">Templomunkról</a>
-<a href="http://www.fabianistva.nhely.hu/kapolna.php">Kápolnánkról</a>
-<a href="http://www.fabianistva.nhely.hu/images.php">Képek</a>
-<a href="http://www.fabianistva.nhely.hu/blog.php">A fília blogja</a>
-<a href="http://www.fabianistva.nhely.hu/usefull.php">Hasznosságok</a>
+<a id="elso" href="http://www.peldaplebania.hu/index.php">Főoldal</a>
+<a href="http://www.peldaplebania.hu/miserend.php">Liturgiák rendje</a>
+<a href="http://www.peldaplebania.hu/history.php">Templomunkról</a>
+<a href="http://www.peldaplebania.hu/kapolna.php">Kápolnánkról</a>
+<a href="http://www.peldaplebania.hu/images.php">Képek</a>
+<a href="http://www.peldaplebania.hu/blog.php">A fília blogja</a>
+<a href="http://www.peldaplebania.hu/usefull.php">Hasznosságok</a>
 <?php
 session_start();
 if (!isset($_SESSION["userId"]))
 {
 	?>
-    <a href="http://www.fabianistva.nhely.hu/imak.php">Imádságok</a>
-    <a href="http://www.fabianistva.nhely.hu/index.php#info">Közérdekű információk</a>
+    <a href="http://www.peldaplebania.hu/imak.php">Imádságok</a>
+    <a href="http://www.peldaplebania.hu/index.php#info">Közérdekű információk</a>
     <?php
 }
 ?>
-<a href="http://www.fabianistva.nhely.hu/login.form.php" id="right-elso" class="right">Bejelentkezés</a>
+<a href="http://www.peldaplebania.hu/login.form.php" id="right-elso" class="right">Bejelentkezés</a>
 </nav>
 <div class="container" onclick="myFunction(this)">
   <div class="bar1"></div>
