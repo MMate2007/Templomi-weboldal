8923790b6f07951dbaf020d84cde7c51c4fc9d6d
Menyhárt Máté <72655257+MMate2007@users.noreply.github.com>
2021-09-05 19:50:02 +0200

    Update login.form.php

--- forraskod/login.form.php
+++ forraskod/login.form.php
@@ -3,14 +3,12 @@
 <title>Bejelentkezés - Példa plébánia</title>
 <meta charset="utf-8">
 <meta name="title" content="Bejelentkezés - Példa plébánia">
-<meta name="description" content="Bejelentkezés után szerkeszteni tudja az oldal bizonyos pontjait. Ha Ön is szeretne segíteni a weboldal jobbá tételében írjon a bszfilia@fabianistva.nhely.hu e-mail címre!">
 <meta name="language" content="hu-HU">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="style.css">
 <link rel="icon" type="image/pnp" href="icon.png">
 <!--<meta name="keywords" content="">-->
 <!--<meta name="theme-color" content="#ffea00">-->
-
 <style>
 header nav a[href="http://<?php echo $_SERVER['HTTP_HOST']; echo $_SERVER['PHP_SELF'];?>"], nav a[href="http://<?php echo $_SERVER['HTTP_HOST']; echo $_SERVER['PHP_SELF'];?>"] {font-weight: bold;}
 @media only screen and (max-width: 600px) {
